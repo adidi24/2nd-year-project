f01f8b98b7033347f68b272a7656cb02105e7952
adidi24 <59664496+adidi24@users.noreply.github.com>
2021-05-07 01:24:58 +0100

    Update Horloge.ino

--- Horloge/Horloge.ino
+++ Horloge/Horloge.ino
@@ -1,23 +1,20 @@
 ```cpp
-
-
-#include <DS1302.h>
+#include "virtuabotixRTC.h"    
 //#include <IRremote.h>  // not now !
-#include <Pushbutton.h> 
-//#include <EEPROM.h> 
+#include <Pushbutton.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
 /*
-  last modification : 05-05-2021
+  last modification : 07-05-2021
   by : Zitouni Abdelkrim
   
   ps : Comment your last modifications to discuss them first
 */
 
 
-DS1302 rtc(8, 7, 6);                  // initialising the DS1302 pins                  
-LiquidCrystal_I2C lcd(0x3F, 16, 2);   // set the @ of the I2C LCD
+virtuabotixRTC myRTC(6, 7, 8);       // initialising the DS1302 pins
+LiquidCrystal_I2C lcd(0x3F, 16, 2);  // set the @ of the I2C LCD
 
 
 
@@ -34,6 +31,10 @@ Pushbutton stopwatchButton(stopwatchButton_pin);
 Pushbutton timerButton(timerButton_pin);
 Pushbutton alarmButton(alarmButton_pin);
 
+
+
+unsigned long clockDisplay = millis();
+
 // time variables
 int CountdownTime = 1;
 int hr = 0;
@@ -41,54 +42,73 @@ int min = 0;
 int setHr = 0;
 int setMin = 0;
 // date variables
-int dy = 1;
-int mon = 1;
-int yr = 2000;
-int setDy = 1;
-int setMon = 1;
-int setYr = 2000;
+int dy;
+int mon;
+int yr;
+int setDy;
+int setMon;
+int setYr;
+int setDOW = 1;
 // alarm variables
 int alarmHr = 0;
 int alarmMin = 0;
-Time::Day DOW = Time::kSunday;
+
 //stopwatch variables
 
 
 
-// enum selection {    // for mode selection (still not working)
-//   OnOff,
-//   Hour,
-//   set_DateTime,
-//   add_Timer,
-//   start_Stopwatch
-  
-// };
+enum selection {  // for mode selection (still not working)
+  Hour,
+  Timer_Count,
+  Alarm,
+  Stopwatch
 
-//enum selection mode = OnOff;
+};
+
+enum selection mode = Hour;
 
 
 //*******************SETUP*******************\\.
 void setup() {
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0); 
-  Serial.begin(9600);  
+  lcd.setCursor(0, 0);
+  Serial.begin(9600);
 
-  rtc.writeProtect(false);
-  rtc.halt(false);
 
-  Time t(2020, 05, 05, 01, 27, 00, Time::kTuesday);
-  rtc.time(t);
+  //myRTC.setDS1302Time(00, 00, 1, 6, 7, 5, 2020);
+  setHr = myRTC.hours;
+  setMin = myRTC.minutes;
+  setDOW = myRTC.dayofweek;
+  setDy = myRTC.dayofmonth;
+  setMon = myRTC.month;
+  setYr = myRTC.year;
 }
 
 
 //////////////////////////////////////////////
 //*******************LOOP*******************\\.
 void loop() {
-  printTime();
-  //modeSelection();
+  mode = Hour;
+  if (millis() - clockDisplay > 500) {
+    myRTC.updateTime();
+    clockDisplay = millis();
+    printTime();
+  }
+
+  switch (mode) {
+    case Hour:
+      setDateTime();
+      //updateTime();
+      //lcd.clear();
+      printTime();
+      break;
+    case Timer_Count:
+      break;
+    case Stopwatch:
+      break;
+  }
   delay(1000);
-  
 }
 //*******************LOOP*******************\\.
 //////////////////////////////////////////////
@@ -97,72 +117,77 @@ void loop() {
 
 
 //function -DayAsString- that converts days of the week into strings
-String DayAsString(const Time::Day day) { 
-  switch (day) {
-    case Time::kSunday: return "Sunday";
-    case Time::kMonday: return "Monday";
-    case Time::kTuesday: return "Tuesday";
-    case Time::kWednesday: return "Wednesday";
-    case Time::kThursday: return "Thursday";
-    case Time::kFriday: return "Friday";
-    case Time::kSaturday: return "Saturday";
+String DayAsString(uint8_t DOW) {
+  switch (DOW) {
+    case 1: return "Sunday";
+    case 2: return "Monday";
+    case 3: return "Tuesday";
+    case 4: return "Wednesday";
+    case 5: return "Thursday";
+    case 6: return "Friday";
+    case 7: return "Saturday";
   }
-  return "inc";
+  return "incorrect";
 }
 
 
 //function -printTime- that prints DateTime to both serial monitor and LCD
 void printTime() {
-  //lcd.clear();
-  Time t = rtc.time();    // Get the current time and date from the DS1302
- 
-  const String day = DayAsString(t.day);   // getting the day of the week
+  lcd.clear();
+
+  const String DOW = DayAsString(myRTC.dayofweek);  // getting the day of the week
 
   // Print everything out to the Serial Monitor window
-  Serial.print(day); Serial.print("  ");
-  Serial.print(t.yr); Serial.print("-");
-  Serial.print(t.mon); Serial.print("-");
-  Serial.print(t.date); Serial.print("  ");
-  Serial.print(t.hr); Serial.print(":");
-  Serial.print(t.min); Serial.print(":");
-  Serial.println(t.sec);
+  Serial.print(DOW);
+  Serial.print("  ");
+  Serial.print(myRTC.year);
+  Serial.print("-");
+  Serial.print(myRTC.month);
+  Serial.print("-");
+  Serial.print(myRTC.dayofmonth);
+  Serial.print("  ");
+  Serial.print(myRTC.hours);
+  Serial.print(":");
+  Serial.print(myRTC.minutes);
+  Serial.print(":");
+  Serial.println(myRTC.seconds);
 
   // Print everything out to the LCD
   lcd.setCursor(4, 0);
-  lcd.print(t.hr);
+  lcd.print(myRTC.hours);
   lcd.setCursor(6, 0);
   lcd.print(":");
   lcd.setCursor(7, 0);
-  lcd.print(t.min);
+  lcd.print(myRTC.minutes);
   lcd.setCursor(9, 0);
   lcd.print(":");
   lcd.setCursor(10, 0);
-  lcd.print(t.sec);
-  
+  lcd.print(myRTC.seconds);
+
   lcd.setCursor(0, 1);
-  lcd.print(day.substring(0, 4));
+  lcd.print(DOW.substring(0, 4));
   lcd.setCursor(6, 1);
-  lcd.print(t.date);
+  lcd.print(myRTC.dayofmonth);
   lcd.setCursor(8, 1);
   lcd.print("/");
   lcd.setCursor(9, 1);
-  lcd.print(t.mon);
+  lcd.print(myRTC.month);
   lcd.setCursor(11, 1);
   lcd.print("/");
   lcd.setCursor(12, 1);
-  lcd.print(t.yr);
+  lcd.print(myRTC.year);
 }
 
-// Function modeSelection : select mode (Date-Time, Timer, StopWhatch) from inputs 
+// Function modeSelection : select mode (Date-Time, Timer, StopWhatch) from inputs
 void modeSelection() {
-  
-  if ((hourButton.getSingleDebouncedPress())) {       // will be change after adding remote control
-    printTime();
-  } else if ((stopwatchButton.getSingleDebouncedPress())) {
-    stopWhatch();
-  } else if ((timerButton.getSingleDebouncedPress())) {
-    Timer();
-  }
+
+  // if ((hourButton.getSingleDebouncedPress())) {       // will be changed after adding remote control
+  //   printTime();
+  // } else if ((stopwatchButton.getSingleDebouncedPress())) {
+  //   stopWhatch();
+  // } else if ((timerButton.getSingleDebouncedPress())) {
+  //   Timer();
+  // }
 }
 
 
@@ -173,132 +198,134 @@ void stopWhatch() {
 
 
 //Function Timer : displays Timer (not completed !)***********
-void Timer() { 
+void Timer() {
   lcd.clear();
 }
 
 
-void setDateTime() { // not completed ! ***********
-
-  Time t = rtc.time();
-  
+// Function setDateTime : setting year, month, day, according to the input
+void setDateTime() {  
   int cursorX = 4, cursorY = 0;
+  while (setButton.isPressed()) {
+    if (timerButton.getSingleDebouncedPress()) {  // from here ..
+      cursorX++;
+      if (cursorX > 7 && cursorY == 0) {
+        cursorY = 1;
+        cursorX = 0;
+      };
+      if (cursorX > 12 && cursorY == 1) {
+        cursorY = 0;
+        cursorX = 4;
+      };
+      lcd.cursor_on();
+      lcd.setCursor(cursorX, cursorY);  // .. to here : we're setting the cursor according to user input
+    }
+    // Here begins the Time setting
+    if (cursorY == 0) {
+      switch (cursorX) {
+        case 4:
+          setHr = myRTC.hours;
+          IncDec(setHr);
+          if (setHr >= 24) setHr = 0;
+          lcd.print(setHr);
 
-  if (mode == Hour) {
-    while (setButton.isPressed()) {
-      if (timerButton.getSingleDebouncedPress()) {
-        cursorX++;
-        if (cursorX > 7 && cursorY == 0) {
-          cursorY = 1;
-          cursorX = 0;
-        };
-        if (cursorX > 12 && cursorY == 1) {
-          cursorY = 0;
-          cursorX = 4;
-        };
-        lcd.cursor_on();
-        lcd.setCursor(cursorX, cursorY);
+          break;
+        case 7:
+          setMin = myRTC.minutes;
+          IncDec(setMin);
+          if (setMin >= 60) setMin = 0;
+          lcd.print(setMin);
+          break;
       }
-      if (cursorY == 0) {   
-        switch (cursorX) {
-          case 4:
-            setHr = t.hr;
-            IncDec(setHr);
-            lcd.print(setHr);
-                    
-            break;
-          case 7:
-            setMin = t.min;
-            IncDec(setMin);
-            lcd.print(setMin);
-            break;   
-        } 
-      } else {
-        switch (cursorX) {
-          case 0:
-            break;
-          case 6:
-            setDy = t.date;
-            IncDec(setDy);
-            if (setDy <= lastDayOfTheMonth(setMon)) lcd.print(setDy); else {
-              setDy = 1; 
-              lcd.print(setDy);
-            }
-                    
-            break;
-          case 9:
-            break;
-          case 12:
-            break;             
-          } 
+    } else {  // Here begins the Date setting
+      switch (cursorX) {
+        case 0:
+          setDOW = myRTC.dayofweek;
+          IncDec(setDOW);
+          if (setDOW > 7) setDOW = 1;
+          lcd.print(DayAsString(setDOW).substring(0, 4));
+          break;
+        case 6:
+          setDy = myRTC.dayofmonth;
+          IncDec(setDy);
+          if (setDy > lastDayOfTheMonth(setMon)) setDy = 1;
+          lcd.print(setDy);
+          break;
+        case 9:
+          setMon = myRTC.month;
+          IncDec(setMon);
+          if (setMon > 12) setMon = 1;
+          lcd.print(setMon);
+          break;
+        case 12:
+          setMon = myRTC.year;
+          IncDec(setYr);
+          if (setYr > 2099) setYr = 2000;
+          lcd.print(setYr);
+          break;
       }
-    } 
-  }      
+    }
+  }
+  myRTC.hours = setHr;
+  myRTC.minutes = setMin;
+  myRTC.dayofweek = setDOW; 
+  myRTC.dayofmonth = setDy;
+  myRTC.month = setMon;
+  myRTC.year = setYr;
+  delay(15);
 }
+///
 
 
-// Function IncDec : increments/decrements any integer by pressing the left/right button respectively 
-void IncDec(int any) {  
-  if (hourButton.getSingleDebouncedPress()) any++; else if (timerButton.getSingleDebouncedPress()) any--;
+// Function IncDec : increments/decrements any integer by pressing the left/Middle button respectively (used to set the clock)
+void IncDec(int any) {
+  if (hourButton.getSingleDebouncedPress()) any++;
+  else if (timerButton.getSingleDebouncedPress())
+    any--;
 }
 
 // Function lastDayOfTheMonth days per month (Leap Year included)
 int lastDayOfTheMonth(int setMonth) {
   switch (setMonth) {
-    case 1: //January
+    case 1:  //January
       return 31;
       break;
-    case 2: //February
-      if ((setYr % 4) == 0) return 29; else return 28;
+    case 2:  //February
+      if ((setYr % 4) == 0) return 29;
+      else
+        return 28;
       break;
-    case 3: //March
+    case 3:  //March
       return 31;
       break;
-    case 4: //April
+    case 4:  //April
       return 30;
       break;
-    case 5: //May
+    case 5:  //May
       return 31;
       break;
-    case 6: //June
+    case 6:  //June
       return 30;
       break;
-    case 7: //JulY
+    case 7:  //JulY
       return 31;
       break;
-    case 8: //August
+    case 8:  //August
       return 31;
       break;
-    case 9: //September
+    case 9:  //September
       return 30;
       break;
-    case 10: //October
+    case 10:  //October
       return 31;
       break;
-    case 11: //November
+    case 11:  //November
       return 30;
       break;
-    case 12: //December
+    case 12:  //December
       return 31;
       break;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ```
